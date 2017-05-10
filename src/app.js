7133a602f1408e140fdf905a9fc04c130e9a33fa
Christofer Ärleryd <c.arleryd@gmail.com>
2017-05-10 17:55:13 +0200

    Fix styling and template for JS implementation

--- src/app.js
+++ src/app.js
@@ -2,33 +2,33 @@ import Rx from "rxjs/Rx";
 import jQuery from "jquery";
 
 
-const refreshButton1 = document.querySelector("#refresh-1");
-const refreshClickStream1 = Rx.Observable.fromEvent(refreshButton1, "click");
+const refreshButton = document.querySelector("#rxjs-refresh");
+const refreshClickStream = Rx.Observable.fromEvent(refreshButton, "click");
 
-const closeButton1 = document.querySelector("#close-1");
-const closeClickStream1 = Rx.Observable.fromEvent(closeButton1, "click");
+const closeButton = document.querySelector("#rxjs-close");
+const closeClickStream = Rx.Observable.fromEvent(closeButton, "click");
 
-const requestStream1 = refreshClickStream1.startWith("startup click")
+const requestStream = refreshClickStream.startWith("startup click")
   .map(() => {
     const randomOffset = Math.floor(Math.random()*500);
     return "https://api.github.com/users?since=" + randomOffset;
   });
 
-const responseStream1 = requestStream1
+const responseStream = requestStream
   .flatMap((requestUrl) => {
     return Rx.Observable.fromPromise(jQuery.getJSON(requestUrl))
   });
 
 
-const suggestion1Stream = closeClickStream1.startWith("startup click")
-  .combineLatest(responseStream1, (click, usersList) => {
+const suggestionStream = closeClickStream.startWith("startup click")
+  .combineLatest(responseStream, (click, usersList) => {
     return usersList[Math.floor(Math.random() * usersList.length)];
   })
-  .merge(refreshClickStream1.map(() => null))
+  .merge(refreshClickStream.map(() => null))
   .startWith(null);
 
 
-suggestion1Stream.subscribe((suggestion) => {
+suggestionStream.subscribe((suggestion) => {
   const loginName = suggestion === null ? "" : suggestion.login;
-  jQuery("#suggestions-1").text(loginName);
+  jQuery("#rxjs-suggestions").text(loginName);
 });
